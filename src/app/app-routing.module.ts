d5552f47761aa2ea1877e341dbcbe34234f25202
shribinwade <84703896+shribinwade@users.noreply.github.com>
2023-11-15 12:15:08 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { FoodPageComponent } from './food-page/food-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
 const routes: Routes = [
 
-  {path:'/', component:HomeComponent},
+  {path:'', component:HomeComponent},
   {path:'search/:searchTerm', component:HomeComponent},
   {path:'tag/:tag',component:HomeComponent},
   {path:'food/:id',component:FoodPageComponent},
